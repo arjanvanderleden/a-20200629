5cda5439bd9b9172519d4e016c025fbea3707c72
Arjan van der Leden <arjan.vander.leden@stepup.nl>
2020-07-01 00:54:18 +0200

    div changes

--- projects/user-registration/src/lib/validators/password-validator.spec.ts
+++ projects/user-registration/src/lib/validators/password-validator.spec.ts
@@ -4,67 +4,86 @@ import { PasswordValidator } from './password-validator';
 import { FormGroup, FormBuilder, ValidatorFn } from '@angular/forms';
 
 
-const formBuilder = new FormBuilder();
-let formGroup: FormGroup
 
+describe('Registration form Validators', () => {
 
-describe('UserRegistrationService', () => {
-    let validator: ValidatorFn;
+    describe("doesNotContainName", () => {
 
-    beforeEach(() => {
-        validator = PasswordValidator.doesNotContainName();
-        formGroup = formBuilder
-            .group({
-                firstName: [''],
-                lastName: [''],
-                password: [''],
-            });
+        const formBuilder = new FormBuilder();
+        let validator: ValidatorFn;
+        let formGroup: FormGroup
+
+        beforeEach(() => {
+            validator = PasswordValidator.doesNotContainName();
+            formGroup = formBuilder
+                .group({
+                    firstName: [''],
+                    lastName: [''],
+                    password: [''],
+                });
+        });
+
+        it('should validate when all values are empty', () => {
+            formGroup.controls['firstName'].setValue('');
+            formGroup.controls['lastName'].setValue('');
+            formGroup.controls['password'].setValue('');
+            expect(validator(formGroup)).toBe(null)
+        })
+
+        it('should validate when password is empty', () => {
+            formGroup.controls['firstName'].setValue('first-name');
+            formGroup.controls['lastName'].setValue('last-name');
+            formGroup.controls['password'].setValue('');
+            expect(validator(formGroup)).toBe(null)
+        })
+
+        it('should validate when password does not contain firstName or lastName', () => {
+            formGroup.controls['firstName'].setValue('first-name');
+            formGroup.controls['lastName'].setValue('last-name');
+            formGroup.controls['password'].setValue('does-not-contain');
+            expect(validator(formGroup)).toBe(null)
+        })
+
+        it('should not validate when password contains firstName case insensitive', () => {
+            formGroup.controls['firstName'].setValue('first-name');
+            formGroup.controls['lastName'].setValue('last-name');
+            formGroup.controls['password'].setValue('-the-FIRST-NAME-');
+            expect(validator(formGroup)).not.toBe(null)
+        })
+
+        it('should not validate when password contains lastName case insensitive', () => {
+            formGroup.controls['firstName'].setValue('first-name');
+            formGroup.controls['lastName'].setValue('last-name');
+            formGroup.controls['password'].setValue('-the-LAST-NAME-');
+            expect(validator(formGroup)).not.toBe(null)
+        })
+
+        it('should not validate when password contains firstName and lastName case insensitive', () => {
+            formGroup.controls['firstName'].setValue('first-name');
+            formGroup.controls['lastName'].setValue('last-name');
+            formGroup.controls['password'].setValue('-the-LAST-NAME-or-FIRST-NAME');
+            expect(validator(formGroup)).not.toBe(null)
+        })
     });
 
-    it('should validate when all values are empty', () => {
-        formGroup.controls['firstName'].setValue('');
-        formGroup.controls['lastName'].setValue('');
-        formGroup.controls['password'].setValue('');
-        expect(validator(formGroup)).toBe(null)
+    describe("hasLength", () => {
+
+        let validator: ValidatorFn;
+
+        beforeEach(() => {
+
+        })
+
+        it('should not validate value = empty', () => {
+
+        })
     })
 
-    it('should validate when password is empty', () => {
-        formGroup.controls['firstName'].setValue('first-name');
-        formGroup.controls['lastName'].setValue('last-name');
-        formGroup.controls['password'].setValue('');
-        expect(validator(formGroup)).toBe(null)
-    })
-
-    it('should validate when password does not contain firstName or lastName', () => {
-        formGroup.controls['firstName'].setValue('first-name');
-        formGroup.controls['lastName'].setValue('last-name');
-        formGroup.controls['password'].setValue('does-not-contain');
-        expect(validator(formGroup)).toBe(null)
-    })
-
-    it('should not validate when password contains firstName case insensitive', () => {
-        formGroup.controls['firstName'].setValue('first-name');
-        formGroup.controls['lastName'].setValue('last-name');
-        formGroup.controls['password'].setValue('-the-FIRST-NAME-');
-        expect(validator(formGroup)).not.toBe(null)
-    })
-
-    it('should not validate when password contains lastName case insensitive', () => {
-        formGroup.controls['firstName'].setValue('first-name');
-        formGroup.controls['lastName'].setValue('last-name');
-        formGroup.controls['password'].setValue('-the-LAST-NAME-');
-        expect(validator(formGroup)).not.toBe(null)
-    })
-
-    it('should not validate when password contains firstName and lastName case insensitive', () => {
-        formGroup.controls['firstName'].setValue('first-name');
-        formGroup.controls['lastName'].setValue('last-name');
-        formGroup.controls['password'].setValue('-the-LAST-NAME-or-FIRST-NAME');
-        expect(validator(formGroup)).not.toBe(null)
-    })
-
-    it('should not validate when', () => {
+    describe("hasLowerCaseAndUpperCase", () => {
 
     })
+
+
+
 
 });
